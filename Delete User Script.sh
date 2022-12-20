6109d5727472071423f9de6afcb701bf98b9f5b0
Parthiban6408 <104077891+Parthiban6408@users.noreply.github.com>
2022-12-20 16:55:48 +0530

    Delete Delete User Script.sh

--- Delete User Script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Welcome to User Deletion Script...!!!"
-
-read -p 'Enter The Username need to be delete: ' name
-
-userdel	 $name
-
-echo "User $name Has Been Created Successfully"
